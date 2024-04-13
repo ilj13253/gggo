b7d8162ce495ef978241af572445da8a2783531e
ilj13253 <butackovilj25@gmail.com>
2024-04-13 23:20:49 +0800

    PRO

---
+++ Server_суефа/Server_суефа.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <Windows.h>
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+void error(const char* msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char* argv[]) {
+    try {
+        setlocale(LC_ALL, "rus");
+        SetConsoleCP(1251);
+        if (argc < 2) {
+            cerr << "Usage: " << argv[0] << " <port>\n";
+            exit(1);
+        }
+
+        WSADATA wsaData;
+        int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        if (iResult != 0) {
+            cerr << "WSAStartup failed: " << iResult << "\n";
+            return 1;
+        }
+
+        SOCKET sockfd, newsockfd;
+        int portno;
+        int clilen; // Изменено на int
+        char buffer[256];
+        struct sockaddr_in serv_addr, cli_addr;
+        int n;
+
+        sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (sockfd == INVALID_SOCKET)
+            error("ERROR opening socket");
+
+        memset((char*)&serv_addr, 0, sizeof(serv_addr));
+        portno = atoi(argv[1]);
+
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        serv_addr.sin_port = htons(portno);
+
+        if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR)
+            error("ERROR on binding");
+
+        if (listen(sockfd, 5) == SOCKET_ERROR)
+            error("ERROR on listen");
+
+        clilen = sizeof(cli_addr); // Изменено на int
+
+        newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
+        if (newsockfd == INVALID_SOCKET)
+            error("ERROR on accept");
+
+        // Начало игры
+
+
+        enum PlayerAction {
+            MOVE = 1,
+            OFFER_DRAW,
+            RESIGN
+        };
+
+        // Внутри основного цикла сервера после accept()
+        int playerAction;
+        recv(newsockfd, (char*)&playerAction, sizeof(playerAction), 0);
+        switch (playerAction) {
+        case MOVE:
+            cout << "Game Mode: Human vs. Computer\n";
+            // Обработка хода
+            break;
+        case OFFER_DRAW:
+            cout << "Game Mode: Computer vs. Computer\n";
+            // Обработка предложения ничьи
+            break;
+        case RESIGN:
+            cout << "Game Mode: Human vs. Human\n";
+            // Обработка признания поражения
+            break;
+        default:
+            cout << "Invalid Game Mode\n";
+            // Некорректный код действия
+            break;
+        }
+        closesocket(newsockfd);
+        closesocket(sockfd);
+        WSACleanup();
+    }
+    catch (exception ex)
+    {
+        cout << ex.what();
+    }
+    return 0;
+}
---
+++ камень-ножницы-бумага/камень-ножницы-бумага.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <string>
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+void error(const char* msg) {
+    std::cerr << msg << ": " << WSAGetLastError() << std::endl;
+    exit(1);
+}
+
+int main(int argc, char* argv[]) {
+    WSADATA wsaData;
+    int wsaStartupResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (wsaStartupResult != 0) {
+        std::cerr << "WSAStartup failed: " << wsaStartupResult << std::endl;
+        return 1;
+    }
+    int sockfd=0;
+    string choiceMessage = "Your choice: ";
+    int choice;
+    cout << "Enter your choice (1-5): ";
+    cin >> choice;
+    choiceMessage += to_string(choice);
+    send(sockfd, choiceMessage.c_str(), choiceMessage.length(), 0);
+
+    // ...
+
+    // Прием результатов игры
+    char resultBuffer[256];
+    recv(sockfd, resultBuffer, 255, 0);
+    cout << "Result: " << resultBuffer << endl;
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <hostname> <port>\n";
+        return 1;
+    }
+    struct addrinfo* addrInfoResult = NULL, * ptr = NULL, hints;
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    int getAddrInfoResult = getaddrinfo(argv[1], argv[2], &hints, &addrInfoResult);
+    if (getAddrInfoResult != 0) {
+        error("getaddrinfo failed");
+    }
+
+    for (ptr = addrInfoResult; ptr != NULL; ptr = ptr->ai_next) {
+        sockfd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (sockfd == INVALID_SOCKET) {
+            error("socket creation failed");
+        }
+
+        if (connect(sockfd, ptr->ai_addr, (int)ptr->ai_addrlen) == SOCKET_ERROR) {
+            closesocket(sockfd);
+            sockfd = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(addrInfoResult);
+
+    if (sockfd == INVALID_SOCKET) {
+        error("Unable to connect to server");
+    }
+
+    // Начало игры
+    // Коды действий игрока
+    enum PlayerAction {
+        MOVE = 1,
+        OFFER_DRAW,
+        RESIGN
+    };
+
+    int choice;
+    cout << "Enter your action:\n";
+    cout << "1. Make a move\n";
+    cout << "2. Offer draw\n";
+    cout << "3. Resign\n";
+    cin >> choice;
+
+    // Проверка на корректность выбранного действия
+
+    send(sockfd, (char*)&choice, sizeof(choice), 0);
+    closesocket(sockfd);
+    WSACleanup();
+    //return 0;
+}
